261146ea381f64ceb23b572770260a9b2ff31ba8
Joel Epstein <joel.epstein2@gmail.com>
2017-07-08 14:05:02 -0700

    added new image subfolders and added mexican resturant images

--- app.js
+++ app.js
@@ -13,4 +13,15 @@ function Restaurant () {
 var thanhSonTofu = new Restaurant ('asian', 'Thanh Son Tofu', '$', '1248 S King St, Seattle, WA 98144', '(206) 320-1316', '8AM to 6PM', '');
 var aPieceOfCake = new Restaurant ('asian', 'A Piece Of Cake', '$', '2401 2nd Ave, Seattle, WA 98121', '(206) 623-8284', '9AM to 9PM', 'apieceofcakeseattle.com');
 var moonlight = new Restaurant ('asian', 'Moon Light Cafe', '$', '1919 S Jackson St, Seattle, WA 98144', '(206) 322-3378', '11:00AM to 9:30PM', '');
-var 
+
+
+
+var = new Restaurant ('mexican', 'Tacos Chukis', '$', '219 Broadway E Seattle, WA 98102', '(206) 328-4447', '11:00AM to 10:00pm', 'https://www.yelp.com/biz/tacos-chukis-seattle?osq=Mexican+Food');
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
+var = new Restaurant ();
