06965942a0fe1ab7e3d841ff0e71ec575e3697cf
hanhthaoluu <hanhthaoluu@gmail.com>
2017-07-09 00:39:35 -0700

    added function getRestaurant

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ var moonLight = new Restaurant ('asian', 'Moon Light Cafe', '$', '1919 S Jackson
 var bambooGarden = new Restaurant ('asian', 'Bamboo Garden Vegetarian', '$$', '364 Roy Street, Seattle, WA 98109', '(206) 282-6616', '11AM to 10PM', 'http://www.bamboo-garden.co/', '');
 var wedgwood = new Restaurant ('asian', 'Wedgwood Vegetarian Thai', '$$', '420 Broadway E., Seattle, WA 98102', '(206) 724-0655', '12:00PM to 9:30PM', 'http://wedgwoodii.com/', '');
 var roti = new Restaurant ('asian', 'Roti Indian Cuisine', '$$', '530 Queen Anne Ave N, Seattle, WA 98109', '(206) 216-7684', '11:30AM to 9:45PM', 'http://rotirestaurantseattle.com/', '');
-var harbor = new Restaurant ('asian', 'Harbor City Restaurant', '$$', '707 S King St., Seattle, WA 98104', '(206) 621-2228', '8:30AM to 11:00PM', '', '');
+var harbor = new Restaurant ('asian', 'Harbor City Restaurant', '$$$', '707 S King St., Seattle, WA 98104', '(206) 621-2228', '8:30AM to 11:00PM', '', '');
 var sevenBeef = new Restaurant ('asian', 'Seven Beef', '$$$', '1305 E Jefferson St., Seattle WA 98122', '(206) 328-7090', '5:00PM to 10:00PM', 'sevenbeef.com', '');
 var shiros = new Restaurant ('asian', 'Shiro\'s', '$$$', '2401 2nd Ave, Seattle WA 98121', '(206) 443-9844', '5:30PM to 10:30PM', 'https://shiros.com/', '');
+
+var restaurants;
+
+restaurants = [thanhSonTofu, aPieceOfCake, moonLight, bambooGarden, wedgwood, roti, harbor, sevenBeef, shiros];
+
+function getRestaurant(category, priceRange){
+  var result = [];
+
+  for(var i = 0; i < restaurants.length; i++){
+    if ((restaurants[i].category === category) && (restaurants[i].priceRange === priceRange)){
+      result.push(restaurants[i]);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+///test function getRestaurant
+var recommendedRestaurant = getRestaurant('asian', '$$$');
+console.log(recommendedRestaurant);
